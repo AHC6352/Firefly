055497cddda084855228ca5294d270d11123bd64
1911whitecell <905061718@qq.com>
2026-06-04 15:43:47 +0800

    删链接

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -73,15 +73,9 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 			{
 				name: "Gitee",
 				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: true,
+				external: false,
 				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
-				external: true,
-				icon: "fa7-brands:qq",
-			},
+			},		
 		],
 	});
 
